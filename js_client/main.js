bb5f40fa3316f0c5333adf5b2647e79248140657
Guillaume Labussiere <labussieremailpro@gmail.com>
2021-01-28 18:41:14 +0100

    prepa de base pour demain

---
+++ js_client/main.js
@@ -0,0 +1,29 @@
+var config = {
+    type : Phaser.AUTO,
+    backgroundColor : "#ccccff",
+    width : 800,
+    height : 600,
+    scene : {
+        preload : preload,
+        create : create,
+        update : update
+    },
+    physics : {
+        default : "arcade",
+        arcade : {
+        gravity : {y : 500}
+        }
+    }
+}
+
+const game = new Phaser.Game(config);
+
+function preload(){
+
+}
+function create(){
+
+}
+function update(time, delta){
+
+}
