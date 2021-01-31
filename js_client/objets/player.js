0ce97ff7d42ee357a144517c3f3fe2233396768a
Jeremia Lateve <jeremia.lateve@gmail.com>
2021-01-31 02:06:19 +0100

    fix

--- js_client/objets/player.js
+++ js_client/objets/player.js
@@ -8,43 +8,56 @@ var player = {
     generatePlayerAnimations: function(){
         jeu.scene.anims.create({
             key : "playerWalk",
-            frames : jeu.scene.anims.generateFrameNames("player",{prefix:"Run", start:1,end:8}),
+            frames : game.anims.generateFrameNames("player",{prefix:"Run", start:1,end:8}),
+            frameRate : 4,
+            repeat : -1 
+        });
+        jeu.scene.anims.create ({
+            key : "playerIdle",
+            frames : [{key : "player",frame : "Idle"}],
+            frameRate : 2,
+            repeat : -1
+        });
+        jeu.scene.anims.create({
+            key : "playerJump",
+            frames : game.anims.generateFrameNames("player",{prefix:"Jump", start:1,end:10}),
             frameRate : 5,
             repeat : -1 
         });
         },
-    gererDeplacement : function(){
+
+     gererDeplacement : function(){
         if(jeu.cursor.left.isDown){
             this.aPlayer.setVelocityX(-200);
             this.aPlayer.setFlip(true,false);
         } else if(jeu.cursor.right.isDown) {
             this.aPlayer.setVelocityX(200);
             this.aPlayer.setFlip(false,false);
-
         }else {
             this.aPlayer.setVelocityX(0);
-        } 
-
+        }
         if(jeu.cursor.up.isDown && this.aPlayer.body.onFloor()) {
-            this.aPlayer.setVelocityY(-400);
-            
+            this.aPlayer.setVelocityY(-350);
         }
 
-        if(this.aPlayer.body.onFloor()) {
+        if(this.aPlayer.body.onFloor()){
             this.isJumping = false;
-        } else{
+        } else {
             this.isJumping = true;
         }
 
-        if(this.isJumping){
-            this.aPlayer.setTexture("player","Jump1")
-        }else{
+        if(this.isJumping === true){
+            if(jeu.cursor.up.isDown)
+            {
+                this.aPlayer.anims.play("playerJump",true);
+            }
+        } else {
             if(jeu.cursor.left.isDown){
                 this.aPlayer.anims.play("playerWalk",true);
-            }else if(jeu.cursor.right.isDown){
-                this.aPlayer.anims.play("playerWalk",true)
-            }else{
-                this.aPlayer.setTexture("playerWalk","Idle")
+            } else if(jeu.cursor.right.isDown) {
+                this.aPlayer.anims.play("playerWalk",true);
+            }else {
+                this.aPlayer.anims.play("playerIdle",true);
             }
         }
     }
